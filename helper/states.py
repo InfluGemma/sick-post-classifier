7cdb1d2940087f3a67d8fb8acca1a504758cf037
rwoodward1414 <rwoodward1414@gmail.com>
2025-07-04 09:02:53 +1000

    Generate reddit posts per region

---
+++ helper/states.py
@@ -0,0 +1,13 @@
+aus_states = ['ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA']
+
+us_regions = {
+    1: ["Connecticut", "Maine", "Massachusetts", "New Hampshire", "Rhode Island", "Vermont"],
+    2: ["New Jersey", "New York", "Pennsylvania"],
+    3: ["Illinois", "Indiana", "Michigan", "Ohio", "Wisconsin"],
+    4: ["Iowa", "Kansas", "Minnesota", "Missouri", "Nebraska", "North Dakota", "South Dakota"],
+    5: ["Delaware", "Florida", "Georgia", "Maryland", "North Carolina", "South Carolina", "Virginia", "West Virginia"],
+    6: ["Alabama", "Kentucky", "Mississippi", "Tennessee"],
+    7: ["Arkansas", "Louisiana", "Oklahoma", "Texas"],
+    8: ["Arizona", "Colorado", "Idaho", "Montana", "Nevada", "New Mexico", "Utah", "Wyoming"],
+    9: ["Alaska", "California", "Hawaii", "Oregon", "Washington"]
+}
