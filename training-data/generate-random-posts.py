a2bf5ca94f47bf36092dbdc1a4712f0503f19a7a
rwoodward1414 <rwoodward1414@gmail.com>
2025-07-23 18:30:46 +1000

    Generated and tagged more data

--- training-data/generate-random-posts.py
+++ training-data/generate-random-posts.py
@@ -17,19 +17,19 @@ base_url = "https://arctic-shift.photon-reddit.com/api/posts/search"
 symptoms = ['flu', 'influenza', 'fever', 'headache', 'sick', 'cough', 'symptom', 'virus']
 
 
-# Grab 100 posts from 7 random subreddits
-for i in range(0, 5):
+# Grab 100 posts from random subredits
+while len(posts) < 100:
     start_year = random.randint(2016, 2024)
     start_month = random.randint(1,12)
     start = datetime(start_year, start_month, 1)
-    end = start + timedelta(days=60)
+    end = start + timedelta(days=50)
 
     start_str = start.strftime("%Y-%m-%d") + "T00%3A00"
     end_str = end.strftime("%Y-%m-%d") + "T00%3A00"
 
     sub = random.choice(list(state_subreddits.values()))
-    # symptom = random.choice(list(symptoms))
-    symptom = "flu"
+    symptom = random.choice(list(symptoms))
+
     url = base_url + "?subreddit=" + sub + "&after=" + start_str + "&before=" + end_str + "&selftext=" + symptom + "&limit=20" 
 
     try:
@@ -41,16 +41,16 @@ for i in range(0, 5):
             text = post.get("selftext", "")
             if text and text != "[removed]" and text != "[deleted]":
                 posts.append((text, sub, start_year))
+        print(f"Got {symptom} posts from {sub}, now at {str(len(posts))}")
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching from {sub}")
 
-# data = {"Text": posts[0], "Sub": posts[1]}
 df = pandas.DataFrame(posts, columns=['text', 'sub', 'year'])
 df['symptoms'] = df['text'].str.contains('|'.join(symptoms), na=False)
 print(df)
 
 # Add label for dataset
-df["Label"] = 0
+df["label"] = 0
 
 df.to_csv('random_posts.csv', mode='a', index=False, sep="|", header=False)
